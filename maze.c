e08ed860055fc86ca90950e291c43458a43ca397
d5npaino <146723372+d5npaino@users.noreply.github.com>
2024-05-01 04:00:24 +0000

    Coursework 2 Part 1

--- maze.c
+++ maze.c
@@ -1,81 +1,239 @@
+/**
+ * @file maze.c
+ * @author (YOUR NAME)
+ * @brief Code for the maze game for COMP1921 Assignment 2
+ * NOTE - You can remove or edit this file however you like - this is just a provided skeleton code
+ * which may be useful to anyone who did not complete assignment 1.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-typedef struct
+// defines for max and min permitted dimensions
+#define MAX_DIM 100
+#define MIN_DIM 5
+
+// defines for the required autograder exit codes
+#define EXIT_SUCCESS 0
+#define EXIT_ARG_ERROR 1
+#define EXIT_FILE_ERROR 2
+#define EXIT_MAZE_ERROR 3
+
+typedef struct __Coord
 {
-   /*This structure stores the 'x and y' position of the player relative to the maze
-   character is defined so that it can be displayed and used to verwrite maze to show where player respectively is*/
-} Character;
+    int x;
+    int y;
+} coord;
 
-typedef struct 
+typedef struct __Maze
 {
-    /*Stores the 2D array of characters for the maze
-    Also stores the che character of the space the player is currently occupying temporarily, so that when the player oves off this space
-    the maze space can be reprinted correcly without losing what that spaced used to be beforehad, as the player position overwrites that space when it moves there
-    maze height and width can also be stored as a numerical value which i useful for loops and iterations and boundary checking for the player etc*/
-} Maze;
+    char **map;
+    int height;
+    int width;
+    coord start;
+    coord end;
+} maze;
 
-
-int main()
+FILE *open_file(char *filename, char *mode)
 {
-/*Starts by asking user to input filename to load maze file, takes their input and stores it as 'filename'
-Calls the 'loadMaze' function, passing the 'filename' through. When done, this should return with a 2D array that tsores the layout of the specified maze
-- now that maze is loaded into a 2D character array and validated, we can begin to use the maze
-While loop with the condition that the maze has not been completed yet (player has not reached the 'E' character)
-Calls the 'displayMaze; function to display the current state of the maze before the player decides where to move if the player wishes to view the map by pressing 'm'
-Calls the 'playerMove' function to move the player. Passed through the current player variable, which is the Character type structure variable
-- Returns from subroutine with an updated character position (most the time).
-If the player has successfully reached the end of the maze in the last movement then the conditional while loop this function is called inside shall be broken as the player has finished
-This should then finish executing the program*/
+    FILE *file = fopen(filename, mode);
+    if (file == NULL)
+    {
+        printf("Error: could not open file\n");
+        return NULL;
+    }
+    return file;
 }
 
-void loadMaze ()
+/**
+ * @brief Initialise a maze object - allocate memory and set attributes
+ *
+ * @param this pointer to the maze to be initialised
+ * @param height height to allocate
+ * @param width width to allocate
+ * @return int 0 on success, 1 on fail
+ */
+int create_maze(maze *this, int height, int width)
 {
-/*
-Calls 'openFile' function with inputted text file to open it so we can access it, pasing 'filename' through and returning a file variable
-Calls 'validateMazeFile' function which passed through the file, and returns the file in a 2D array of characters
-Returns this 2D array from where this function was called
-*/
 }
 
-FILE *openFile(char *filename, char *mode){
-/*Attemps to open file with passed filename and mode to specify usage
-Error handling such that if filename is invalid, an exception is thrown 
-If file sucessfully opened and read, it is returned*/
-}
-
-char* validateMazeFile (FILE *file)
+/**
+ * @brief Free the memory allocated to the maze struct
+ *
+ * @param this the pointer to the struct to free
+ */
+void free_maze(maze *this)
 {
-/*Begins by opening and reading first line of file inside of a while loop
-First check height/width of maze against desired parameters (between 5-100 characters)
-Checks that each character is valid, and records lenght of row, if an S/E character as been used etc
-Validates each line through specified rules, such as making sure each row is the same character lenghth
-Checks that the S/E char havent been used more than once
---> note that when S character has been read, its 'position' is saved and stored inside the players initial starting position 
-This repeats inside the while loop for each line until the file ha been completely read
-then completes check that each column has teh same number of characters (make sure each line is the same length)
-Exception is thrown if any of these conditions arent met, but if maze file is valid, it is stored inside a 2D character array
-this array is then returned back to the main function*/
+    free(this);
 }
 
-void playerMove (Character player, Maze maza)
+/**
+ * @brief Validate and return the width of the mazefile
+ *
+ * @param file the file pointer to check
+ * @return int 0 for error, or a valid width (5-100)
+ */
+int get_width(FILE *file)
 {
-    /*Takes user input as 'movement' in the form of W/A/S/D key input. Validates correct character input otherwise an exception is thrown
-    dependant on the users inputted 'movement', the players x and y values are updated (incrementation) depewndant on their move choice
-    This new movement is then checked against the maze, and if the move is invalid an appropriate message shall appear
-    telling the player they cannot move to that space (e.g. tried to move onto a # space)
-    Given the move is valid, the players 'X' character should be updated on the maze, and the space they were previously on restored
-    back to the character it was (this is stored temporarily inside a variable inside the 'Maze' structure)
-    Finally, if the player has successfully moved onto the end position of the maze, a variable shoudl be returned/updated to show that
-    the maze has been completed (alike a boolean) which is linked to the conditional while loop inside the main function*/
 }
 
-void displayMaze (Maze maze)
+/**
+ * @brief Validate and return the height of the mazefile
+ *
+ * @param file the file pointer to check
+ * @return int 0 for error, or a valid height (5-100)
+ */
+int get_height(FILE *file)
 {
-/*Prints the 2D array of characters on the screen in the correct maze format so the player can see a
-visual representation of not only the maze, but where there character currently is respective to the maze layout
-- Reminder: This should only display upon the players input of 'm'*/
 }
 
+/**
+ * @brief read in a maze file into a struct
+ *
+ * @param this Maze struct to be used
+ * @param file Maze file pointer
+ * @return int 0 on success, 1 on fail
+ */
+int read_maze(maze *this, FILE *file)
+{
+    char line[MAX_DIM];
+    int counter = 0;
+    int tempWidth;
+    int startFound = 0;
+    int endFound = 0;
+    if (file != NULL)
+    {
+        while (fgets(line, MAX_DIM, file) != NULL)
+        {
+
+            if (strlen(line) < 5 || strchr(line, '\n') == NULL)
+            {
+                printf("Error: invalid maze format\n");
+                return 3;
+            }
+            else if (counter == 0)
+            {
+                tempWidth = strlen(line);
+            }
+            else if (strlen(line) != tempWidth)
+            {
+                printf("Error: invalid maze format\n");
+                return 3;
+            }
+
+            for (int i = 0; i < strlen(line); i++)
+            {
+                if (line[i] == 'S')
+                {
+                    startFound++;
+                    this->start.x = i;
+                    this->start.y = counter;
+                }
+                else if (line[i] == 'E')
+                {
+                    endFound++;
+                    this->end.x = i;
+                    this->end.y = counter;
+                }
+                else if (line[i] != ' ' || line[i] != '#' || line[i] != '\n')
+                {
+                    printf("Error: invalid maze format\n");
+                    return 3;
+                }
+            }
+
+            this->map[counter] = line;
+            counter++;
+        }
+        if (endFound != 1 || startFound != 1)
+        {
+            printf("Error: invalid maze format\n");
+            return 3;
+        }
+        counter = this->height;
+        tempWidth = this->width;
+    }
+    else
+    {
+        printf("Error: could not open file\n");
+        return 2;
+    }
+}
+
+/**
+ * @brief Prints the maze out - code provided to ensure correct formatting
+ *
+ * @param this pointer to maze to print
+ * @param player the current player location
+ */
+void print_maze(maze *this, coord *player)
+{
+    // make sure we have a leading newline..
+    printf("\n");
+    for (int i = 0; i < this->height; i++)
+    {
+        for (int j = 0; j < this->width; j++)
+        {
+            // decide whether player is on this spot or not
+            if (player->x == j && player->y == i)
+            {
+                printf("X");
+            }
+            else
+            {
+                printf("%c", this->map[i][j]);
+            }
+        }
+        // end each row with a newline.
+        printf("\n");
+    }
+}
+
+/**
+ * @brief Validates and performs a movement in a given direction
+ *
+ * @param this Maze struct
+ * @param player The player's current position
+ * @param direction The desired direction to move in
+ */
+void move(maze *this, coord *player, char direction)
+{
+}
+
+/**
+ * @brief Check whether the player has won and return a pseudo-boolean
+ *
+ * @param this current maze
+ * @param player player position
+ * @return int 0 for false, 1 for true
+ */
+int has_won(maze *this, coord *player)
+{
+}
+
+int main(int argNum, char *args[])
+{
+    // check args
+    if (argNum < 2)
+    {
+        printf("Please input filename after running /maze.c\n");
+        return 1;
+    }
+
+    // set up some useful variables (you can rename or remove these if you want)
+    coord *player;
+    maze *thisMaze = malloc(sizeof(maze));
+    FILE *file = open_file(args[1], "r");
+    read_maze(thisMaze, file);
+
+    // open and validate mazefile
+
+    // read in mazefile to struct
+
+    // maze game loop
+
+    // win
+
+    // return, free, exit
+}
